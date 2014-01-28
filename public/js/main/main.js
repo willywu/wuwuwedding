4ec36ae9d8f30e3a4feda69b2b312db6d6f333b9
Willy Wu <wu62086@gmail.com>
2014-01-28 00:37:25 -0800

    First pass at a google map, also add an analytics tag

--- public/js/main/main.js
+++ public/js/main/main.js
@@ -1,3 +1,22 @@
 function goToByScroll(id){
     $('html,body').stop().animate({scrollTop: ($("#"+id).offset().top)-=50},{duration: 1500, easing: "easeInOutExpo"});
 }
+function initialize() {
+    var mapOptions = {
+      zoom: 13,
+      center: new google.maps.LatLng(37.7792, -122.4191),
+      zoomControl: true,
+      mapTypeControl: false
+    };
+
+    var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+}
+
+function loadScript() {
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&callback=initialize&key=AIzaSyDpXihQIfDZCUZkAypRBCsqqOYvbRUEwTQ';
+    document.body.appendChild(script);
+}
+
+window.onload = loadScript;
