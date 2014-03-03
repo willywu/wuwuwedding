7f423a5f154f4f208e29d1d6b2c8097e2946c518
Willy Wu <wu62086@gmail.com>
2014-03-02 16:15:14 -0800

    First pass at a 'wedding' section

--- public/js/main/main.js
+++ public/js/main/main.js
@@ -1,12 +1,13 @@
 function goToByScroll(id){
     $('html,body').stop().animate({scrollTop: ($("#"+id).offset().top)-=42},{duration: 1500, easing: "easeInOutExpo"});
 }
+
 function initialize() {
     var mapOptions = {
-      zoom: 17,
-      center: new google.maps.LatLng(37.7792, -122.419),
-      zoomControl: true,
-      mapTypeControl: false
+            zoom: 17,
+            center: new google.maps.LatLng(37.7792, -122.419),
+            zoomControl: true,
+            mapTypeControl: false
     };
 
     var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
@@ -19,24 +20,24 @@ function loadMapScript() {
     document.body.appendChild(script);
 }
 
+function confirmRsvp() {
+    $('#rsvp-success').fadeIn('slow');
+}
+
 function runPageLoadedScripts() {
-  loadMapScript();
-  $('#rsvp-button').click(function() {
-    $('#rsvp-success').fadeOut('slow');
-  });
+    //loadMapScript();
+    $('#rsvp-button').click(function() {
+        $('#rsvp-success').fadeOut('slow');
+    });
 }
 
 window.onload = runPageLoadedScripts;
 
 $.get('photo_section', function(data) {
-    $('#photos').append(data)
+    $('#photos').append(data);
     $(window).load(function() {
         $('#slider').nivoSlider({
-          effect: 'fade'
+            effect: 'fade'
         });
     });
 });
-
-function confirmRsvp() {
-  $('#rsvp-success').fadeIn('slow');
-}
