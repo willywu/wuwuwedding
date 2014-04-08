1686a9eec05ff890b3de32853e76b22752f26be5
Willy Wu <wu62086@gmail.com>
2014-04-07 22:56:14 -0700

    Add a postgresql database, switch out the main photos

--- project/Build.scala
+++ project/Build.scala
@@ -9,11 +9,11 @@ object ApplicationBuild extends Build {
 
     val appDependencies = Seq(
       // dependencies defined by group, artifact, and revision
-      "com.newrelic.agent.java" % "newrelic-agent" % "3.0.1"
+      "com.newrelic.agent.java" % "newrelic-agent" % "3.0.1",
+      "postgresql" % "postgresql" % "9.1-901.jdbc4"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
-      // Add your own project settings here
     )
 
 }
