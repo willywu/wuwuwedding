59d4bcc431642d62b228415401e76bc161022a27
Willy Wu <wu62086@gmail.com>
2014-04-09 22:20:12 -0700

    Make rsvp codes have a length of 5

--- python/rsvp-code-generator.py
+++ python/rsvp-code-generator.py
@@ -3,11 +3,13 @@ import random
 """Generate unique RSVP codes for the wedding"""
 
 if __name__=="__main__":
-    chars = "BCDFGHJKLMNPQRSTVWXYZ23456789"
+    chars = "BCDEFGHJKLMNPQRTVWXY346789" # exclude (0,O), (1,I), (2,Z), (5,S)
     items = [x for x in chars]
 
     out_filename = "codes.txt"
     with open(out_filename, 'w') as out_file:
+        codes = []
         for i in range(100):
-            code = ''.join([random.choice(items) for i in range(8)]) # codes are 8 chars long
-            out_file.write(code+"\n")
+            codes.append(''.join([random.choice(items) for i in range(5)])) # codes are 8 chars long
+        codes.sort()
+        out_file.write('\n'.join(codes))
